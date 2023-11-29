5bd1834e09fb6dd560fcb4dae67793c244d07fc6
JGabrielNeto <joao.gabrielsn1997@gmail.com>
2023-11-29 20:25:52 -0300

    feat: adding prisma and configuring routes and database

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+  IsPositive,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsPositive()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsNotEmpty()
+  price: number;
+}
